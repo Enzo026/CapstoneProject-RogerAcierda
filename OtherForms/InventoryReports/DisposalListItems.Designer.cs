347c36b42d75a8b71681b3d0f3d151f1e9a1ed41
EnzoDT026 <lorenzo.lorenzo2626@gmail.com>
2024-10-07 08:59:11 +0800

    hmm

---
+++ OtherForms/InventoryReports/DisposalListItems.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Flowershop_Thesis.OtherForms.InventoryReports
+{
+    partial class DisposalListItems
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel27 = new System.Windows.Forms.Panel();
+            this.EmpLbl = new System.Windows.Forms.Label();
+            this.button30 = new System.Windows.Forms.Button();
+            this.StatusLbl = new System.Windows.Forms.Label();
+            this.DateLbl = new System.Windows.Forms.Label();
+            this.ItemsLbl = new System.Windows.Forms.Label();
+            this.CustNameLbl = new System.Windows.Forms.Label();
+            this.IdLbl = new System.Windows.Forms.Label();
+            this.panel27.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel27
+            // 
+            this.panel27.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel27.Controls.Add(this.EmpLbl);
+            this.panel27.Controls.Add(this.button30);
+            this.panel27.Controls.Add(this.StatusLbl);
+            this.panel27.Controls.Add(this.DateLbl);
+            this.panel27.Controls.Add(this.ItemsLbl);
+            this.panel27.Controls.Add(this.CustNameLbl);
+            this.panel27.Controls.Add(this.IdLbl);
+            this.panel27.Location = new System.Drawing.Point(3, 3);
+            this.panel27.Name = "panel27";
+            this.panel27.Size = new System.Drawing.Size(1039, 30);
+            this.panel27.TabIndex = 21;
+            // 
+            // EmpLbl
+            // 
+            this.EmpLbl.AutoSize = true;
+            this.EmpLbl.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmpLbl.Location = new System.Drawing.Point(803, 7);
+            this.EmpLbl.Name = "EmpLbl";
+            this.EmpLbl.Size = new System.Drawing.Size(34, 15);
+            this.EmpLbl.TabIndex = 15;
+            this.EmpLbl.Text = "Enzo";
+            // 
+            // button30
+            // 
+            this.button30.BackColor = System.Drawing.Color.Salmon;
+            this.button30.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button30.Font = new System.Drawing.Font("Montserrat Medium", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button30.Location = new System.Drawing.Point(902, 3);
+            this.button30.Name = "button30";
+            this.button30.Size = new System.Drawing.Size(117, 23);
+            this.button30.TabIndex = 14;
+            this.button30.Text = "View full details";
+            this.button30.UseVisualStyleBackColor = false;
+            // 
+            // StatusLbl
+            // 
+            this.StatusLbl.AutoSize = true;
+            this.StatusLbl.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.StatusLbl.Location = new System.Drawing.Point(668, 7);
+            this.StatusLbl.Name = "StatusLbl";
+            this.StatusLbl.Size = new System.Drawing.Size(70, 15);
+            this.StatusLbl.TabIndex = 13;
+            this.StatusLbl.Text = "Evaluating";
+            // 
+            // DateLbl
+            // 
+            this.DateLbl.AutoSize = true;
+            this.DateLbl.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DateLbl.Location = new System.Drawing.Point(478, 7);
+            this.DateLbl.Name = "DateLbl";
+            this.DateLbl.Size = new System.Drawing.Size(105, 15);
+            this.DateLbl.TabIndex = 12;
+            this.DateLbl.Text = "11/07/24 | 12:34 pm";
+            // 
+            // ItemsLbl
+            // 
+            this.ItemsLbl.AutoSize = true;
+            this.ItemsLbl.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ItemsLbl.Location = new System.Drawing.Point(338, 7);
+            this.ItemsLbl.Name = "ItemsLbl";
+            this.ItemsLbl.Size = new System.Drawing.Size(13, 15);
+            this.ItemsLbl.TabIndex = 10;
+            this.ItemsLbl.Text = "3";
+            // 
+            // CustNameLbl
+            // 
+            this.CustNameLbl.AutoSize = true;
+            this.CustNameLbl.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustNameLbl.Location = new System.Drawing.Point(124, 7);
+            this.CustNameLbl.Name = "CustNameLbl";
+            this.CustNameLbl.Size = new System.Drawing.Size(39, 15);
+            this.CustNameLbl.TabIndex = 9;
+            this.CustNameLbl.Text = "Alexis";
+            // 
+            // IdLbl
+            // 
+            this.IdLbl.AutoSize = true;
+            this.IdLbl.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdLbl.Location = new System.Drawing.Point(3, 7);
+            this.IdLbl.Name = "IdLbl";
+            this.IdLbl.Size = new System.Drawing.Size(18, 15);
+            this.IdLbl.TabIndex = 8;
+            this.IdLbl.Text = "01";
+            // 
+            // DisposalListItems
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.panel27);
+            this.Name = "DisposalListItems";
+            this.Size = new System.Drawing.Size(1046, 36);
+            this.panel27.ResumeLayout(false);
+            this.panel27.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel27;
+        private System.Windows.Forms.Label EmpLbl;
+        private System.Windows.Forms.Button button30;
+        private System.Windows.Forms.Label StatusLbl;
+        private System.Windows.Forms.Label DateLbl;
+        private System.Windows.Forms.Label ItemsLbl;
+        private System.Windows.Forms.Label CustNameLbl;
+        private System.Windows.Forms.Label IdLbl;
+    }
+}
---
+++ OtherForms/InventoryReports/DisposalListItems.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flowershop_Thesis.OtherForms.InventoryReports
+{
+    public partial class DisposalListItems : UserControl
+    {
+        public DisposalListItems()
+        {
+            InitializeComponent();
+        }
+
+        #region Myregion
+        private string ItemID;
+        private string ID;
+        private string CustomerName;
+        private string EmployeeName;
+        private string TotalQuantity;
+        private string Status;
+        private string Date;
+
+        [Category("ItemList")]
+        public string TransID
+        {
+            get { return ItemID; }
+            set { ItemID = value; }
+        }
+        [Category("ItemList")]
+        public string LocalID
+        {
+            get { return ID; }
+            set { ID = value; IdLbl.Text = value.ToString(); }
+        }
+        [Category("ItemList")]
+        public string CustName
+        {
+            get { return CustomerName; }
+            set { CustomerName = value; CustNameLbl.Text = value.ToString(); }
+        }
+        [Category("ItemList")]
+        public string Qty
+        {
+            get { return TotalQuantity; }
+            set { TotalQuantity = value; ItemsLbl.Text = value.ToString() + " Php"; }
+        }
+        [Category("ItemList")]
+        public string Employee
+        {
+            get { return EmployeeName; }
+            set { EmployeeName = value; EmpLbl.Text = value.ToString(); }
+        }
+        [Category("ItemList")]
+        public string status
+        {
+            get { return Status; }
+            set { Status = value; StatusLbl.Text = value.ToString(); }
+        }
+        public string date
+        {
+            get { return Date; }
+            set { Date = value; DateLbl.Text = value.ToString(); }
+        }
+        #endregion
+    }
+}
